fce411922537af54a9743af9cee20901b1d11b53
kimyurie <9911dbfl@naver.com>
2023-09-25 22:08:26 +0900

    Design: 네비게이션 / 상품 이미지

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Container, Nav, Navbar} from 'react-bootstrap';
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import bg from './img/bg.png';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
-      <Navbar bg="dark" data-bs-theme="dark">
+       <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">Shop</Navbar.Brand>
           <Nav className="me-auto">
@@ -17,6 +17,33 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
+      {/* html에서 이미지 넣을 때 */}
+      <div className='main-bg' style={{backgroundImage : 'url(' + bg + ')'}}></div>
+    
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            {/* 수백개 이미지 넣을 때 - public 폴더 이용 이미지 넣기 */}
+            {/* <img src="/logo192.png" width="80%"/> */}
+            {/* 아래가 이미지 넣는 권장 방식 - codingapple.com/어쩌구/ 경로에 배포시 */}
+            <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
+    
     </div>
   );
 }
