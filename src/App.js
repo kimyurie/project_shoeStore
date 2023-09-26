a6c58e8744c945bf47d5f5bf416c624326754ee7
kimyurie <9911dbfl@naver.com>
2023-09-26 16:38:00 +0900

    Feat: 페이지 이동(navigate, nested routes, outlet)

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import {CardGroup, Container, Nav, Navbar} from 'react-bootstrap';
 import bg from './img/bg.png';
 import { useState } from 'react';
 import data from './data';
-import {Routes, Route, Link} from 'react-router-dom';
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
 import Detail from './routes/detail';
 
 function App() {
   // 상품 데이터 넣기 - 많은 데이터는 따로 data.js 에 저장해서 불러오는 방식으로
+  // use~ => 훅 (유용한 것들 들어있는 함수같은거)
   let [shoes] = useState(data);
+  let navigate = useNavigate();  // 페이지 이동 도와줌
 
   return (
     <div className="App">
-      
 
        <Navbar bg="light" data-bs-theme="light">
         <Container>
@@ -23,8 +24,9 @@ function App() {
             {/* 페이지 이동 버튼 */}
             {/* <Link to="/">홈</Link>
             <Link to="/detail">상세페이지</Link> */}
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features"> Cart</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/')}}>Home</Nav.Link>
+            {/* navigate(-1)은 뒤로가기 1은 앞으로 가기 */}
+            <Nav.Link onClick={() => {navigate('/detail')}}> Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -65,12 +67,51 @@ function App() {
             </>
           </div>}/>
           <Route path='/detail' element= {<Detail/>}/>
+          {/* 404페이지 */} {/* *은 이외 모든 것 */}
+          <Route path='*' element= {<div>없는 페이지에요</div>}/> 
+
+          {/* Nested Routes - 아래 코드와 동일 */}
+            {/* <Route path='/about/member' element = {<About/>}/>
+          <Route path='/about/location' element = {<About/>}/> */} 
+
+          {/* 언제씀 ? 여러 유사한 페이지 필요할 때 / 동적인 ui */}
+         <Route path='/about' element = {<About/>}>
+             <Route path='member' element = {<div>멤버임</div>}/>
+             <Route path='location' element = {<div>위치정보임</div>}/>
+          </Route>    
+
+        <Route path='/event' element = {<EventPage/>}>
+            <Route path='one' element = {<p>첫 주문시 양배추즙 서비스</p>}/>
+            <Route path='two' element = {<p>생일기념 쿠폰받기</p>}/>
+          </Route>
+
       </Routes>
 
+ 
+         
+    
 
-      
     </div>
   );
+}
+
+function EventPage(){
+  return(
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+
+function About(){
+  return (
+    <div>
+      <h4>회사 정보임</h4>
+      <Outlet></Outlet> {/* 위 Nested routes의 element 보여줄 자리*/}
+    </div>
+  )
 }
 
 function Card(props){
